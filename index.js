d96d09afe1e54cc4c46a10a2e15e6444026c5256
SARAN0209 <108873756+SARAN0209@users.noreply.github.com>
2022-12-16 22:09:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express=require("express");
+const employeerouter=require("./EmployeeRouter/router")
+const mango =require("./connect");
+const dotenv=require("dotenv");
+const employeeregsiter=require("./EmployeeRouter/regsiterrouter")
+// const auth =require("./Employeemodule/authmodules");
+const cors=require("cors")
+
+
+dotenv.config();
+const app =express();
+app.use(express.json())
+mango.connect();
+// app.use(cors());
+app.use(cors({origin:"*",credentials : true}))
+app.use("/register",employeeregsiter);
+
+// app.use("/",auth.authuser)
+app.use("/employee",employeerouter)
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,()=>{
+    console.log(`App is running on PORT ${PORT}`)
+});
