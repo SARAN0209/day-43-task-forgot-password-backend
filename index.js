069fc4b4bd6391e7b839ae880013fa389c9d6886
SARAN0209 <108873756+SARAN0209@users.noreply.github.com>
2023-01-02 14:55:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const employeerouter=require("./EmployeeRouter/router")
 const mango =require("./connect");
 const dotenv=require("dotenv");
 const employeeregsiter=require("./EmployeeRouter/regsiterrouter")
-// const auth =require("./Employeemodule/authmodules");
+const auth =require("./Employeemodule/authmodules");
 const cors=require("cors")
 
 
@@ -11,11 +11,11 @@ dotenv.config();
 const app =express();
 app.use(express.json())
 mango.connect();
-// app.use(cors());
+app.use(cors());
 app.use(cors({origin:"*",credentials : true}))
 app.use("/register",employeeregsiter);
 
-// app.use("/",auth.authuser)
+app.use("/",auth.authuser)
 app.use("/employee",employeerouter)
 const PORT = process.env.PORT || 5000
 
